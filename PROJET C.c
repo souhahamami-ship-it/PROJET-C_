f8a709fcfb4bb59ccb24b3bb4637f5041ba6e2cc
souhahamami-ship-it <souha.hamami@ensi-uma.tn>
2025-12-10 21:31:44 +0000

    tache 3 4 et  ajouter commande intial

--- PROJET C.c
+++ PROJET C.c
@@ -1,13 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+void journaliser(const Commande *c, const char *action){
+    FILE *f=fopen("journal_robot.txt","a"); if(!f) return;
+    fprintf(f,"[%.2fs] Prio: %s | Type: %s | Dur: %.1f | Vit: %.1f | Action: %s\n",
+        temps_simulation, c->priorite, c->type, c->duree, c->vitesse, action);
+    fprintf(f,"POSITION : x=%d y=%d\n", robot_longitudinal_pos, robot_lateral_pos);
+    fclose(f);
+}
+void executer_commande(Commande c){
+    if(strcmp(c.priorite,URGENTE)==0){
+        printf("COMMANDE URGENTE : %s\n",c.type);
+        temps_simulation+=DUREE_CYCLE_BASE;
+        journaliser(&c,"EXECUTION URGENTE");
+        afficher_interface();
+        return;
+    }}
+void gerer_obstacle(float duree_tourner, float vitesse_tourner, float duree_avancer, float vitesse_avancer){
+    Commande c;
+    strcpy(c.type,TOURNER_DROITE); c.duree=duree_tourner; c.vitesse=vitesse_tourner; strcpy(c.priorite,NORMALE);
+    ajouter_commande_normale(c);
+    strcpy(c.type,AVANCER); c.duree=duree_avancer; c.vitesse=vitesse_avancer; strcpy(c.priorite,NORMALE);
+    ajouter_commande_normale(c);
+    strcpy(c.type,TOURNER_GAUCHE); c.duree=duree_tourner; c.vitesse=vitesse_tourner; strcpy(c.priorite,NORMALE);
+    ajouter_commande_normale(c);
+    strcpy(c.type,AVANCER); c.duree=duree_avancer/2; c.vitesse=vitesse_avancer; strcpy(c.priorite,NORMALE);
+    ajouter_commande_normale(c);
+    printf("* Contournement de l'obstacle ajoutÃ© ***\n");
+}
+void ajouter_commandes_initiales(float duree_avancer, float vitesse_avancer, float duree_tourner, float vitesse_tourner){
+    Commande c;
+    strcpy(c.type,AVANCER); c.duree=duree_avancer; c.vitesse=vitesse_avancer; strcpy(c.priorite,NORMALE);
+    ajouter_commande_normale(c);
+    strcpy(c.type,TOURNER_DROITE); c.duree=duree_tourner; c.vitesse=vitesse_tourner; strcpy(c.priorite,NORMALE);
+    ajouter_commande_normale(c);
+    strcpy(c.type,AVANCER); c.duree=duree_avancer/2; c.vitesse=vitesse_avancer; strcpy(c.priorite,NORMALE);
+    ajouter_commande_normale(c);
+    strcpy(c.type,TOURNER_GAUCHE); c.duree=duree_tourner; c.vitesse=vitesse_tourner; strcpy(c.priorite,NORMALE);
+    ajouter_commande_normale(c);
+}
 int main(){
     init_file(&file_normale); init_file(&file_urgente);
     FILE *f=fopen("journal_robot.txt","w"); if(f){fprintf(f,"======= Journal Robot =======\n"); fclose(f);}
     float duree_avancer = 5, vitesse_avancer = 1;
     float duree_tourner = 2, vitesse_tourner = 1;
     ajouter_commandes_initiales(duree_avancer,vitesse_avancer,duree_tourner,vitesse_tourner);
-    printf("=== Simulation Robot Début ===\n");
+    printf("=== Simulation Robot Dï¿½but ===\n");
     const int MAX_CYCLES=50; int cycle=0;
     Commande c_exec;
     while((!file_vide(&file_normale)||!file_vide(&file_urgente)) && cycle<MAX_CYCLES){
@@ -22,8 +60,8 @@ int main(){
         cycle++;
     }
 
-    if(cycle>=MAX_CYCLES) printf("=== Simulation arrêtée après %d cycles ===\n",MAX_CYCLES);
-    else printf("=== Simulation Robot Terminé ===\n");
+    if(cycle>=MAX_CYCLES) printf("=== Simulation arrï¿½tï¿½e aprï¿½s %d cycles ===\n",MAX_CYCLES);
+    else printf("=== Simulation Robot Terminï¿½ ===\n");
 
     return 0;
 }
